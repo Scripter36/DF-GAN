e10f83705117ab98352a1ae81e70cf73547e022b
Scripter36 <1350adwx@gmail.com>
2022-12-10 02:56:05 +0900

    Create python_install.sh

---
+++ python_install.sh
@@ -0,0 +1,12 @@
+apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
+libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
+xz-utils tk-dev liblzma-dev
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+source ~/.bashrc
+
+pyenv install 3.9.13
+pyenv global 3.9.13
+pip install -r requirements.txt -f https://download.pytorch.org/whl/torch_stable.html
