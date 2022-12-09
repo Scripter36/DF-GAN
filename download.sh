08f6167eb008f5ef6d4bd3966e168017a1a41e46
Scripter36 <1350adwx@gmail.com>
2022-12-10 03:55:04 +0900

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+apt install unzip
 FILEID="1I6ybkR7L64K8hZOraEZDuHh0cCJw5OUj"
 FILENAME="./data/birds.zip"
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=FILEID' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$FILEID" -O $FILENAME && rm -rf /tmp/cookies.txt
@@ -8,8 +9,8 @@ FILENAME="./data/coco.zip"
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=FILEID' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$FILEID" -O $FILENAME && rm -rf /tmp/cookies.txt
 
 cd ./data
-unzip ./data/birds.zip
-unzip ./data/coco.zip
+unzip birds.zip
+unzip coco.zip
 
 FILEID="1IWqTeFbQcx7yzqAghGEABQ1cmh6bfxOK"
 FILENAME="./CUB_200_2011.tgz"
