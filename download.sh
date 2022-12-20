cabd3ccb89baacb3c35cfa62601e77b90a35c883
Scripter36 <1350adwx@gmail.com>
2022-12-20 11:44:42 +0900

    Update download.sh

--- download.sh
+++ download.sh
@@ -15,4 +15,4 @@ FILEID="1IWqTeFbQcx7yzqAghGEABQ1cmh6bfxOK"
 FILENAME="./CUB_200_2011.tgz"
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=FILEID' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$FILEID" -O $FILENAME && rm -rf /tmp/cookies.txt
 
-tar -xvf CUB_200_2011.tgz -C ./birds/
+tar -xf CUB_200_2011.tgz -C ./birds/
