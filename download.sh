c2b192e292e678bcfd8a4be23c84bd632ce9fea0
Scripter36 <1350adwx@gmail.com>
2022-12-20 11:34:53 +0900

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-apt install unzip
 FILEID="1I6ybkR7L64K8hZOraEZDuHh0cCJw5OUj"
 FILENAME="./data/birds.zip"
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=FILEID' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$FILEID" -O $FILENAME && rm -rf /tmp/cookies.txt
