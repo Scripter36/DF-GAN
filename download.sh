59d692cb7cdd3bc1e837817aed9a4a727c511158
Scripter36 <1350adwx@gmail.com>
2022-12-20 11:49:55 +0900

    Update download.sh to download pretrained models

--- download.sh
+++ download.sh
@@ -16,3 +16,17 @@ FILENAME="./CUB_200_2011.tgz"
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=FILEID' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$FILEID" -O $FILENAME && rm -rf /tmp/cookies.txt
 
 tar -xf CUB_200_2011.tgz -C ./birds/
+
+cd ../code
+mkdir saved_models
+cd saved_models
+FILEID="1rzfcCvGwU8vLCrn5reWxmrAMms6WQGA6"
+FILENAME="./pretrained_bird.zip"
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=FILEID' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$FILEID" -O $FILENAME && rm -rf /tmp/cookies.txt
+
+FILEID="1e_AwWxbClxipEnasfz_QrhmLlv2-Vpyq"
+FILENAME="./pretrained_coco.zip"
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=FILEID' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$FILEID" -O $FILENAME && rm -rf /tmp/cookies.txt
+
+unzip -q pretrained_bird.zip
+unzip -q pretrained_coco.zip
